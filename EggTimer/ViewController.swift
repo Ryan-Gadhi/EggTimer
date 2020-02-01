fc57150c18d538caeb148ee2d173c276e0a5bb09
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-02-01 23:02:57 +0300

    first commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let levels = ["Soft": 5, "Medium": 8, "Hard": 12]
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let level = sender.currentTitle
+    
+        print(levels[level!])
+    }
+    
+
+}
