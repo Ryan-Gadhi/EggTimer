4511261c22b844c2896cf1eab09320186852ded6
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-02-03 02:05:40 +0300

    added: timer, update labal when eggs are ready and updated background style

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,15 +11,45 @@ import UIKit
 class ViewController: UIViewController {
     
     let levels = ["Soft": 5, "Medium": 8, "Hard": 12]
+    var counter = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var labelUpdate: UILabel!
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let level = sender.currentTitle
     
-        print(levels[level!])
+        print(levels[level!]!)
         print("minutes")
+        let eggTimeInMinutes = levels[level!]
+        let timeInSeconds = Int(eggTimeInMinutes!) * 1 // 60 seconds in  one minute
+        countDown(count: Int(timeInSeconds)) // should do it in better way
+    
     }
+    
+    func countDown(count: Int){
+        counter = count
+        timer.invalidate() // this will stop the timer, so that the 3 buttons don't overlap
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
+    }
+
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("time remainging until your egg is ready is \(counter) seconds")
+            counter -= 1
+        }else{
+            print("your eggs are ready 😇")
+            labelUpdate.text = "Horray! your eggs are ready 😍"
+            timer.invalidate()
+        }
+    }
+        
+    
+  
     
 
 }
