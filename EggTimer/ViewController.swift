87bfa38a494cfb33fe6a083e5c9ab8e4eb447fca
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-02-03 02:55:01 +0300

    added an alarm functionality, when the eggs finsih, the alarm will go off

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
@@ -14,6 +16,7 @@ class ViewController: UIViewController {
     var counter = 0
     var timer = Timer()
     var totalTime = 0
+    var player: AVAudioPlayer?
     
     @IBOutlet weak var labelUpdate: UILabel!
     
@@ -52,9 +55,32 @@ class ViewController: UIViewController {
             timer.invalidate()
             
             increasedProgress.progress = 1
+            playSound()
         }
     }
         
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
   
     
